b497eaf08daa39eb08c01322dbe5c4231a410324
davis_chang <davis chang>
2018-10-29 22:21:37 +0800

    isDragDisabled

--- src/Task.jsx
+++ src/Task.jsx
@@ -7,7 +7,12 @@ const Container = styled.div`
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: ${props => props.isDragging ? 'lightgreen' : 'white'};
+  background-color: ${props =>
+    props.isDragDisabled
+      ? 'lightgrey'
+      : props.isDragging
+        ? 'lightgreen'
+        : 'white'};
 
   display: flex;
 `;
@@ -26,10 +31,14 @@ const Container = styled.div`
 
 export default class Task extends React.Component {
   render() {
+
+    const isDragDisabled = this.props.task.id === 'task-1';
+
     return (
       <Draggable
         draggableId={this.props.task.id}
         index={this.props.index}
+        isDragDisabled={isDragDisabled}
       >
         {
           (provided, snapshot) => (
@@ -38,6 +47,7 @@ export default class Task extends React.Component {
               {...provided.draggableProps}
               {...provided.dragHandleProps}
               isDragging={snapshot.isDragging}
+              isDragDisabled={isDragDisabled}
             >
               {this.props.task.content}
             </Container>
